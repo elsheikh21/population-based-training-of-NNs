67a4209d0fc7683a960b7da9804c448fd892d082
Ahmed ElSheikh <ahmedhelsheikh@gmail.com>
2019-06-13 22:06:28 +0200

    saving plot

--- main.py
+++ main.py
@@ -176,6 +176,7 @@ ax.plot(best_accuracy_hist)
 ax.plot(nonreg_accuracy_hist, c='red')
 ax.set(xlabel='Hundreds of training iterations', ylabel='Test accuracy')
 ax.legend(['Population based training', 'Non-regularized baseline'])
+plt.savefig('100s.png')
 plt.show()
 
 
@@ -184,4 +185,5 @@ ax = f.add_subplot(1, 1, 1)
 ax.plot(2 ** l1_scale_hist.T)
 ax.set_yscale('log')
 ax.set(xlabel='Hundreds of training iterations', ylabel='L1 regularizer scale')
+plt.savefig('scale_100s.png')
 plt.show()
